62e26ce60f471429312f184ad5ad2c4067c081d9
lupini95 <31459620+lupini95@users.noreply.github.com>
2017-10-31 14:59:37 -0600

    Update HW5.sql

--- HW5.sql
+++ HW5.sql
@@ -1,135 +1,84 @@
 CREATE DATABASE airline_booking;
 
 \c airline_booking
--- Costumer Table with auto
--- generated primary key 'costumer_id'
-CREATE TABLE costumers(
-	costumer_id SERIAL PRIMARY KEY,
-	last_name VARCHAR(100),
-	first_name VARCHAR(100));
 
--- Countries Table
+-- countries table
 CREATE TABLE countries(
-	country_name VARCHAR(100),
-	country_code INT,
-	country_abr VARCHAR(10),
-	PRIMARY KEY (country_code));
+	country_name VARCHAR(100) UNIQUE,
+	country_code INT UNIQUE,
+	country_abr VARCHAR(10) UNIQUE,
+	PRIMARY KEY (country_abr));
+
+-- airlines table
+CREATE TABLE airlines(
+	airline_name VARCHAR(25),
+	country_abr VARCHAR(10) REFERENCES countries(country_abr),
+	airline_code VARCHAR(10),
+	PRIMARY KEY (airline_code));
 
 -- States Table
 CREATE TABLE states_provinces(
-	state_province_name VARCHAR(225),
-	state_province_abr VARCHAR(20),
+	state_province_name VARCHAR(225) UNIQUE,
+	state_province_abr VARCHAR(20) UNIQUE,
 	PRIMARY KEY (state_province_abr));
 
 -- Cities Table
 CREATE TABLE cities(
 	city_name VARCHAR(100),
-	city_code VARCHAR(10), --airport can use this, may be null
+	city_code VARCHAR(10) UNIQUE, --airport can use this, may be null
 	state_province_abr VARCHAR(10) REFERENCES states_provinces(state_province_abr),
-	country_code INT REFERENCES countries (country_code),
-	CONSTRAINT pk_city PRIMARY KEY (city_name, state_province_abr, country_code));
+	country_abr VARCHAR(10) REFERENCES countries(country_abr),
+	CONSTRAINT pk_city PRIMARY KEY (city_name, state_province_abr));
 
--- Area_Codes Table
-CREATE TABLE area_codes(
-	city_name VARCHAR(100),
-	state_province_abr VARCHAR(10),
-	area_code INT,
-	country_code INT,
-	CONSTRAINT pk_area PRIMARY KEY (area_code, country_code),
-	CONSTRAINT fk_area
-		FOREIGN KEY (city_name, state_province_abr, country_code)
-		REFERENCES cities(city_name, state_province_abr, country_code));
+-- area code table
+CREATE TABLE area_code(
+	area_code INT PRIMARY KEY,
+	state_province_abr VARCHAR(20) REFERENCES states_provinces(state_province_abr));
 
--- Postal_Codes Table
-CREATE TABLE postal_codes(
-	postal_code INT,
-	city_name VARCHAR(100),
-	state_province_abr VARCHAR(10),
-	country_code INT,
-	CONSTRAINT pk_postal PRIMARY KEY (postal_code,city_name),
-	CONSTRAINT fk_postal
-		FOREIGN KEY (city_name, state_province_abr, country_code)
-		REFERENCES cities(city_name, state_province_abr, country_code));
+-- Phone_Number Table
+CREATE TABLE phone_number(
+	full_num VARCHAR(11) PRIMARY KEY,
+	country_code INT REFERENCES countries(country_code),
+	area_code INT REFERENCES area_code(area_code), 
+	local_num INT);
+
+-- costumer table
+CREATE TABLE costumers(
+	costumer_id SERIAL PRIMARY KEY,
+	last_name VARCHAR(100),
+	first_name VARCHAR(100),
+	phone VARCHAR(11) REFERENCES phone_number(full_num));
 
 -- Address Table
 CREATE TABLE address(
-	street_num INT NOT NULL,
-	apt_num INT,
-	street_name VARCHAR(225) NOT NULL,
-	postal_code INT NOT NULL,
+	street_num INT,
+	street_name VARCHAR(225),
 	city_name VARCHAR(100),
-	CONSTRAINT pk_address PRIMARY KEY (street_num, apt_num, street_name, postal_code, city_name),
+	state_province_abr VARCHAR(10),
+	postal_code INT,
+	CONSTRAINT pk_address
+		PRIMARY KEY (street_num, street_name, city_name, state_province_abr),
 	CONSTRAINT fk_address 
-		FOREIGN KEY (postal_code, city_name)
-		REFERENCES postal_codes(postal_code, city_name));
+		FOREIGN KEY (city_name, state_province_abr)
+		REFERENCES cities(city_name, state_province_abr));
 
 -- Lives_At Relationship Table
 CREATE TABLE lives_at(
 	costumer_id INT REFERENCES costumers(costumer_id),
-	street_num INT NOT NULL,
-	apt_num INT,
-	street_name VARCHAR(225) NOT NULL,
-	postal_code INT NOT NULL,
+	street_num INT,
+	street_name VARCHAR(225),
 	city_name VARCHAR(100),
+	state_province_abr VARCHAR(10),
 	CONSTRAINT pk_lives_at 
-		PRIMARY KEY (costumer_id, street_num, apt_num, street_name, postal_code, city_name),
+		PRIMARY KEY (costumer_id, street_num, street_name, city_name, state_province_abr),
 	CONSTRAINT fk_lives_at 
-		FOREIGN KEY (street_num, apt_num, street_name, postal_code, city_name)
-		REFERENCES address(street_num, apt_num, street_name, postal_code, city_name));
-
--- Phone_Number Table
-CREATE TABLE phone_number(
-	country_code INT,
-	area_code INT,
-	local_num INT,
-	CONSTRAINT pk_phone PRIMARY KEY (country_code, area_code, local_num),
-	CONSTRAINT fk_phone 
-		FOREIGN KEY (country_code, area_code)
-		REFERENCES area_codes(country_code, area_code));
-
--- has_num Table
-CREATE TABLE has_num(
-	costumer_id INT REFERENCES costumers(costumer_id),
-	country_code INT,
-	area_code INT,
-	local_num INT,
-	CONSTRAINT pk_has_num PRIMARY KEY (costumer_id, area_code, local_num),
-	CONSTRAINT fk_has_num 
-		FOREIGN KEY (country_code, area_code, local_num)
-		REFERENCES phone_number(country_code, area_code, local_num));
-
--- email table
-CREATE TABLE email(
-	costumer_id INT REFERENCES costumers(costumer_id),
-	email VARCHAR(225),
-	CONSTRAINT pk_email PRIMARY KEY (costumer_id, email));
-
--- airlines table
-CREATE TABLE airlines(
-	airline_name VARCHAR(25),
-	country_abr VARCHAR(10),
-	airline_code VARCHAR(10),
-	PRIMARY KEY (airline_code));
-
--- city_codes
-CREATE TABLE travel_locations(
-	city_name VARCHAR(100),
-	state_province_abr VARCHAR(10) REFERENCES states_provinces(state_province_abr),
-	country_code INT REFERENCES countries(country_code),
-	city_code VARCHAR(10),
-	PRIMARY KEY (city_code));
-
-CREATE TABLE booking_locations(
-	city_name VARCHAR(100),
-	state_province_abr VARCHAR(10) REFERENCES states_provinces(state_province_abr),
-	country_code INT REFERENCES countries(country_code),
-	city_code VARCHAR(10),
-	PRIMARY KEY (city_code));
+		FOREIGN KEY (street_num, street_name, city_name, state_province_abr)
+		REFERENCES address(street_num, street_name, city_name, state_province_abr));
 
 -- flights table
 CREATE TABLE flights(
-	origin_city_code VARCHAR(10) REFERENCES travel_locations(city_code),
-	dest_city_code VARCHAR(10) REFERENCES travel_locations(city_code),
+	origin_city_code VARCHAR(10) REFERENCES cities(city_code),
+	dest_city_code VARCHAR(10) REFERENCES cities(city_code),
 	unique_flight_num INT,
 	airline_code VARCHAR(10) REFERENCES airlines(airline_code),
 	flight_length TIME(0),
@@ -141,7 +90,7 @@ CREATE TABLE flights(
 -- bookings table
 CREATE TABLE bookings(
 	booking_id INT,
-	city_code VARCHAR(10) REFERENCES booking_locations(city_code),
+	city_code VARCHAR(10) REFERENCES cities(city_code),
 	book_date DATE,
 	payers_id INT REFERENCES costumers(costumer_id),
 	unique_flight_num INT REFERENCES flights(unique_flight_num),
@@ -152,5 +101,3 @@ CREATE TABLE tickets(
 	booking_id INT REFERENCES bookings(booking_id),
 	passenger_id INT REFERENCES costumers(costumer_id),
 	CONSTRAINT pk_ticket PRIMARY KEY (booking_id, passenger_id));
-
-
